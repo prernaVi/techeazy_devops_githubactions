a9fa8cd84641a14a740752c2918f2204a92195dd
prernaVi <prernaupadhyay.505@gmail.com>
2025-07-21 18:37:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,22 @@
 provider "aws" {
-  region = var.aws_region
+  region = "ap-south-1"
 }
 
-resource "aws_vpc" "techeazy_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = { Name = "techeazy-vpc" }
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_subnet" "techeazy_subnet" {
-  vpc_id                  = aws_vpc.techeazy_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "ap-south-1a"
-  tags = { Name = "techeazy-subnet" }
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 resource "aws_security_group" "instance_sg" {
-  name        = "techeazy-sg"
+  name        = "techeazy-instance-sg"
   description = "Allow SSH and HTTP"
-  vpc_id      = aws_vpc.techeazy_vpc.id
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     description = "SSH"
@@ -42,64 +40,36 @@ resource "aws_security_group" "instance_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = { Name = "techeazy-sg" }
-}
-
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = var.bucket_name
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "log_lifecycle" {
-  bucket = aws_s3_bucket.log_bucket.id
-
-  rule {
-    id     = "log-expiration"
-    status = "Enabled"
-
-    expiration {
-      days = 7
-    }
-
-    filter {} # Add this line to satisfy the provider requirement
-  }
-}
-
-resource "aws_iam_role" "ec2_role" {
-  name = "techeazy-ec2-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = { Service = "ec2.amazonaws.com" },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_s3_access" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "techeazy-ec2-profile"
-  role = aws_iam_role.ec2_role.name
 }
 
 resource "aws_instance" "techeazy_instance" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.techeazy_subnet.id
+  ami                    = "ami-0447a12f28fddb066"
+  instance_type          = "t2.micro"
+  key_name               = "prerna-key-new"
+  subnet_id              = data.aws_subnets.default.ids[0]
   vpc_security_group_ids = [aws_security_group.instance_sg.id]
-  key_name               = var.key_name
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
-  user_data = file("${path.module}/scripts/setup.sh")
+  tags = {
+    Name = "techeazy-devops-instance"
+  }
 
-  tags = { Name = "techeazy-ec2" }
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install java-openjdk11 -y
+              yum install git -y
+              cd /home/ec2-user
+              git clone https://github.com/prernaVi/techeazy_devops_githubactions.git app
+              cd app
+              chmod +x mvnw
+              ./mvnw package
+              java -jar target/*.jar --server.port=80
+              EOF
 }
+
+output "instance_public_ip" {
+  value = aws_instance.techeazy_instance.public_ip
+}
+
 
 
